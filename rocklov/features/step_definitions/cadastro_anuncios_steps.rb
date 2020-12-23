69e932ff0dc90a086fee149e4668799557cec162
Petherson Erasmo <petherson_erasmo@hotmail.com>
2020-12-23 20:22:44 -0300

    Corrigindo problema causado na atualização da versão do contêiner.
    Quando atualizamos a versão do rocklov-web o dev corrigiu o erro de digitação no campo do placeholder que estava e-email. Então, atualizamos o campo e percebemos que se tivéssemos muitos arquivos o impacto para manutenção seria grande. Como solução usamos o padrão Page Object no arquivo login_pages.rb.

--- rocklov/features/step_definitions/cadastro_anuncios_steps.rb
+++ rocklov/features/step_definitions/cadastro_anuncios_steps.rb
@@ -1,10 +1,23 @@
 Dado('que estou logado como {string} e {string}') do |email, password|
     @email = email # usado quando chamamos o método MongoDB
-    visit "/"      
-    find("input[placeholder='Seu e-email']").set email
-    find("input[type=password]").set password
+    # Quando atualizamos a versão do rocklov-web o dev corrigiu o erro de digitação no campo do placeholder que estava e-email
+    # Então, atualizamos o campo e percebemos que se tivéssemos muitos arquivos o impacto para manutenção seria grande.
+    # Como solução usamos o padrão Page Object no arquivo login_pages.rb
+    # visit "/"      
+    # find("input[placeholder='Seu email']").set email
+    # find("input[type=password]").set password
+    # click_button "Entrar"
 
-    click_button "Entrar"
+    # Estou estanciando a classe LoginPage e guardando na variável login_page
+    login_page = LoginPage.new
+    # Na classe LoginPage já programamos para entrar na página, agora basta chamar o método.
+    login_page.abrir_pagina
+    # Na classe LoginPage já programamos para encontrar o campo email, agora basta chamar o método e informar o valor que queremos.
+    login_page.campo_email.set email
+    # Na classe LoginPage já programamos para encontrar o campo senha, agora basta chamar o método e informar o valor que queremos.
+    login_page.campo_senha.set password
+    # Na classe LoginPage já programamos para clicar no botão, agora basta chamar o método.
+    login_page.botao_entrar
 end                                                                          
                                                                                
 Dado('que acesso o fomulário de cadastro de anúncio') do
