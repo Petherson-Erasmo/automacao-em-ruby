6a28b29efb855a8d074e70227f2f15461113b950
Petherson Erasmo <petherson_erasmo@hotmail.com>
2020-12-23 18:17:55 -0300

    Configurando o tempo máx de espera por um elemento.
    Como foi feita a atualização do contêiner rocklov-web para outraversão (v2), os testes pararam de funcionar e uns dos problemas é que agora está demorando mais para entrar na dashboard, por causa desse problema configuramos para o capybara esperar até 10s por um elemento.

--- rocklov/features/support/env.rb
+++ rocklov/features/support/env.rb
@@ -7,4 +7,5 @@ Capybara.configure do |config|
     # se eu deixar apenas com o :selenium vai ser utilizado o firefox
     config.default_driver = :selenium_chrome
     config.app_host = "http://rocklov-web:3000"
+    config.default_max_wait_time = 10 # define o tempo máx que o capybara vai esperar por um elemento na tela
 end
