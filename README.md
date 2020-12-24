ad8ed23133a2f1ce8b7b4123702c681a7c946a10
Petherson Erasmo <petherson_erasmo@hotmail.com>
2020-12-24 18:22:12 -0300

    Restruturando todo o projeto.
    Tomei a decisão de separar o módulo do rocklov em duas pastas já que tivemos atualizações no contêiner. Agora a pasta rocklov-v2 é a versão que vamos utilizar nela excluímos os comentários antigos para o projeto não ficar poluído visualmente. No arquivo login_pages.rb adotamos o padrão app actions junto com o padrão page objects. Na pasta rocklov voltamos para a última aula antes da atualização que foi implementação do método get_user e remove_equipment. Mexemos no arquivo readme para explicar a função de cada página. Tentei mais uma vez configurar o arquivo gitignore para ignorar os arquivos de log do mongo.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # automacao-em-ruby
 Este projeto é referente ao curso de automação 180 da QA NInja. Foi criado testes automatizados para web e para API REST, utilizando o Cucumber, Ruby, Capybara, RSpec, Jenkins, especificações em BDD.
+
+Cada pasta tem uma função, a pasta code4teste é utilizada para introduzir conceitos da programação em Ruby. A pasta web-base introduz alguns conceitos em HTML. A pasta rocklov tem a primeira versão do projeto. Decidi cria a pasta rocklov-v2, pois, como atualizamos a versão do contêiner e estou fazendo anotações no código para explicar o funcionamento, achei válido criar essa pasta com os arquivos já feitos, mas sem os comentários que, já tinha feito para facilitar a leitura do código e não ficar poluído.
--- rocklov/features/step_definitions/login_steps.rb
+++ rocklov/features/step_definitions/login_steps.rb
@@ -1,27 +1,12 @@
 Dado('que acesso a página principal') do
-    # visit "/" # passamos a usar a rota padrão no arquivo env.rb
-
-    # Estou estanciando a classe LoginPage e guardando na variável login_page
-    @login_page = LoginPage.new
-    # Na classe LoginPage já programamos para entrar na página, agora basta chamar o método.
-    @login_page.abrir_pagina
+    visit "/" # passamos a usar a rota padrão no arquivo env.rb
 end
   
 Quando('submeto minhas credenciais com {string} e {string}') do |email, password|
-    # Quando atualizamos a versão do rocklov-web o dev corrigiu o erro de digitação no campo do placeholder que estava e-email
-    # Então, atualizamos o campo e percebemos que se tivéssemos muitos arquivos o impacto para manutenção seria grande.
-    # Como solução usamos o padrão Page Object no arquivo login_pages.rb
-    # Como solução utilizamos o padrão page object 
-    # find("input[placeholder='Seu email']").set email
-    # find("input[type=password]").set password
-    # click_button "Entrar"
+    find("input[placeholder='Seu e-email']").set email
+    find("input[type=password]").set password
 
-    # Na classe LoginPage já programamos para encontrar o campo email, agora basta chamar o método e informar o valor que queremos.
-    @login_page.campo_email.set email
-    # Na classe LoginPage já programamos para encontrar o campo senha, agora basta chamar o método e informar o valor que queremos.
-    @login_page.campo_senha.set password
-    # Na classe LoginPage já programamos para clicar no botão, agora basta chamar o método.
-    @login_page.botao_entrar
+    click_button "Entrar"
 end
 
 # O step Então está sendo usada no arquivo shared_steps.rb já que é o mesmo comportamento dos aquivos cadastro e login.
